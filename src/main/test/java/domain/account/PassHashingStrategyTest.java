c338c74e9a5c07c47c95631ab44670df60c419b5
Sivar72 <sivar.werrie@student.hu.nl>
2018-03-12 08:34:47 +0100

    web.xml

--- src/main/test/java/domain/account/PassHashingStrategyTest.java
+++ src/main/test/java/domain/account/PassHashingStrategyTest.java
@@ -1,12 +1,9 @@
 package domain.account;
 
-import domain.account.IPassHashingStrategy;
-import domain.account.PassHashingStrategyImpl;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.mindrot.jbcrypt.BCrypt;
 import utils.Pair;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -30,8 +27,6 @@ class PassHashingStrategyTest {
     @ValueSource(strings = {"password", "Test1235", "hMsJWXlx5Yq8"})
     void testPassEquals(String password) {
         Pair<String, String> hashedPair = hashingStrategy.hashPassword(password);
-        System.out.println(hashedPair.getLeft());
-        System.out.println(BCrypt.);
         assertTrue(hashingStrategy.passwordEquals(password, hashedPair.getRight(), hashedPair.getLeft()));
     }
 
