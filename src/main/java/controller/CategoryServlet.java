a3ddf29a129cb64f4ec73e19aca112034a53b80c
Sivar72 <sivar.werrie@student.hu.nl>
2018-03-12 17:09:10 +0100

    Index include

--- src/main/java/controller/CategoryServlet.java
+++ src/main/java/controller/CategoryServlet.java
@@ -2,6 +2,7 @@ package controller;
 
 import utils.ServletUtils;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,9 +16,15 @@ public class CategoryServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
+        try {
+            request.getRequestDispatcher("/index.jsp").include(request, response);
+        } catch (ServletException e) {
+            e.printStackTrace();
+        }
 
         PrintWriter out = response.getWriter();
         out.println("<h1>Category with id: " + ServletUtils.getPathId(request.getPathInfo()) + "</h1>");
+        out.close();
     }
 
 }
