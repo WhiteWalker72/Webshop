d8e49edd2b83bfbcf3bb007266ec8bd0a4e73b9d
Briggy238 <brigitte.leblanc@student.hu.nl>
2018-03-20 14:17:09 +0100

    Login and create account servlets done for table account.

--- src/main/java/controller/servlet/AccountServlet.java
+++ src/main/java/controller/servlet/AccountServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Date;
+
 import domain.account.PassHashingStrategyImpl;
 
 import domain.account.Account;
@@ -14,7 +15,7 @@ import exceptions.ObjectAlreadyExistsException;
 import persistence.PersistenceServices;
 import utils.Pair;
 
-@WebServlet(value="/Account")
+@WebServlet(value = "/Account")
 public class AccountServlet extends HttpServlet {
 
     @Override
@@ -29,12 +30,19 @@ public class AccountServlet extends HttpServlet {
         String username = req.getParameter("username");
         String psw = req.getParameter("psw");
 
-        Pair<String, String> veiligWachtwoord = new PassHashingStrategyImpl().hashPassword(psw);
+        if (PersistenceServices.getInstance().getAccount(username) != null) {
+            req.getRequestDispatcher("/createaccount.jsp").forward(req, resp);
+            return;
+        }
 
+        Pair<String, String> saltPassPair = new PassHashingStrategyImpl().hashPassword(psw);
         try {
-            PersistenceServices.getInstance().insertAccount(new Account(0, new Date(), 1, username, veiligWachtwoord.getLeft(), veiligWachtwoord.getRight()));
+            PersistenceServices.getInstance().insertAccount(new Account(Integer.parseInt(PersistenceServices.getInstance().getNextAccountId())
+                    , new Date(), 1, username, saltPassPair.getLeft(), saltPassPair.getRight()));
         } catch (ObjectAlreadyExistsException e) {
-            e.printStackTrace();
+            // req.getRequestDispatcher("/createaccount.jsp").forward(req, resp);
+            resp.sendRedirect("/createaccount.jsp");
+            return;
         }
 
         req.getRequestDispatcher("/accountCreated.jsp").forward(req, resp);
